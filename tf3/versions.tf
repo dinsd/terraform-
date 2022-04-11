1777da76eea43af7745033fbe08ef8f334d2af86
deecloud <deearzieva@gmail.com>
2022-04-11 00:25:32 -0400

    1 2 3 all

---
+++ tf3/versions.tf
@@ -0,0 +1,13 @@
+# tf version
+# aws version
+terraform {
+  required_version = "~> 1.1.0"   #tf version
+  required_providers {             #provider=aws version
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.7.0"   
+  }
+}
+}
+
+

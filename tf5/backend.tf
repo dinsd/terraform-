7250a2891f889da44aab27e67d8f673d1bcf9a0f
deecloud <deearzieva@gmail.com>
2022-04-19 19:29:23 -0400

    tf5 update

--- tf5/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-      bucket = "tfusers3"
-      key = "tf5/terraform.tfstate"
-      region = "us-east-1"
-      dynamodb_table = "tflocktable" # for state locking
-    }
-}

97b400d0bc811c467da1b81c4c515895f5ee0fe1
dinsd <102567512+dinsd@users.noreply.github.com>
2022-04-18 21:05:05 -0400

    Delete tf9 directory

--- tf9/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-      bucket = "tfusers3"
-      key = "tf9/qa/terraform.tfstate"
-      region = "us-east-1"
-      dynamodb_table = "tflocktable"
-    }
-}
--- tf9/qa /versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = "~> 1.1.0"   #tf version
-  required_providers {             #provider=aws version
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.7.0"   
-  }
-}
-}
